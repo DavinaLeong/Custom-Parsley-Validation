07b7cdecd6d54ad5983d47dfe90a678af107197f
Davina Leong <davina.leong@outlook.com>
2018-02-09 10:55:37 +0800

    Created model to interface with Cpv dump table.

---
+++ application/models/Cpv_dump_model.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cpv_dump_model extends CI_Model {
+    const TABLE_NAME = 'cpv_dump';
+
+    public function get_all($order_by_col='dump_timestamp', $direction='DESC') {
+        $this->db->order_by($order_by_col, $direction);
+        $query = $this->db->get($this::TABLE_NAME);
+        return $query->result_array();
+    }
+
+    public function get_by_dump_timestamp($dump_timestamp=FALSE, $order_by_col='form_field', $direction='DESC') {
+        if($dump_timestamp && is_string($dump_timestamp)) {
+            $this->db->order_by($order_by_col, $direction);
+            $query = $this->db->get_where($this::TABLE_NAME, array('dump_timestamp' => $dump_timestamp));
+            return $query->result_array();
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function insert($cpv_dump=FALSE) {
+        if($cpv_dump && is_array($cpv_dump)) {
+
+            $temp = [
+                'form_field' => $cpv_dump['form_field'],
+                'field_value' => $cpv_dump['field_value'],
+                'dump_timestamp' => $cpv_dump['dump_timestamp']
+            ];
+
+            $this->load->library('Datetime_formatter');
+            $this->db->set('timestamp', $this->datetime_formatter->now(MYSQL_DATETIME_FORMAT));
+            $this->db->insert($this::TABLE_NAME, $temp);
+            return $this->db->insert_id();
+
+        } else {
+            return FALSE;
+        }
+    }
+
+} //end Cpv_dump_model class
